dd3e8e7a2fafdfc345281e8a4b22d57f969c2ca2
Mark Brezina <77184504+MarkBrezina@users.noreply.github.com>
2026-03-12 08:32:09 +0000

    Update matching_engine.py

--- src/matching_engine.py
+++ src/matching_engine.py
@@ -1,1 +1,22 @@
+def match_market_order(order_book, size, side):
 
+    trades = []
+
+    if side == "buy":
+        book_side = sorted(order_book.asks.items())
+
+    else:
+        book_side = sorted(order_book.bids.items(), reverse=True)
+
+    for price, liquidity in book_side:
+
+        traded = min(size, liquidity)
+
+        trades.append((price, traded))
+
+        size -= traded
+
+        if size == 0:
+            break
+
+    return trades
