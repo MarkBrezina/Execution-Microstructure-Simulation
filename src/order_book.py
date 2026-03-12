8ca544770ae0848e5e1c41d35d235d25f8137b51
Mark Brezina <77184504+MarkBrezina@users.noreply.github.com>
2026-03-12 08:31:57 +0000

    Update order_book.py

--- src/order_book.py
+++ src/order_book.py
@@ -1,1 +1,16 @@
 
+class OrderBook:
+
+    def __init__(self):
+        self.bids = {}
+        self.asks = {}
+
+    def add_order(self, price, size, side):
+
+        if side == "bid":
+            self.bids.setdefault(price, 0)
+            self.bids[price] += size
+
+        else:
+            self.asks.setdefault(price, 0)
+            self.asks[price] += size
